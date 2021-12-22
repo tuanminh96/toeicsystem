5fb8b7c37fc61c3a34c43137ec2251b7a01e9d93
Thangdz998 <levanthang12nls@gmail.com>
2021-12-22 22:34:41 +0700

    [ToeicSystem] Update them code kk.

--- src/main/webapp/resources/js/client/baiTestReading.js
+++ src/main/webapp/resources/js/client/baiTestReading.js
@@ -28,6 +28,8 @@ $(document).ready(function () {
                 clearInterval(interval);
                 $('#mainContent').html(html);
                 $('.modal-backdrop').removeClass("modal-backdrop fade show");
+                $('.modal-open').removeAttr("style");
+                $('.modal-open').removeClass("modal-open");
             }, error: function (html) {
                 alert('Error');
                 $('#mainContent').html(html.responseText);
